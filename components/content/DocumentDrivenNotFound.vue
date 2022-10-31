91dd5a0fa401adcd0e494a88d9290ed0ee89b903
Marshall <57112423+Xy2002@users.noreply.github.com>
2022-10-31 16:17:28 +0800

    feat(content): add 404 page

---
+++ components/content/DocumentDrivenNotFound.vue
@@ -0,0 +1,12 @@
+<template>
+	<div>
+		<div class="flex items-center justify-center flex-col">
+			<p class="text-primary text-3xl lg:text-6xl font-black">
+				404 not found
+			</p>
+			<NuxtLink to="/" class="bg-primary text-white rounded px-4 py-4">
+				Go back home
+			</NuxtLink>
+		</div>
+	</div>
+</template>
