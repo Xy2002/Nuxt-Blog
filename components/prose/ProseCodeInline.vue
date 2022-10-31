984bbe3a570489ce7d8fb6a73fa1de00c7e9964b
Marshall <57112423+Xy2002@users.noreply.github.com>
2022-10-31 15:32:43 +0800

    style(code inline): add new style for code inline

---
+++ components/prose/ProseCodeInline.vue
@@ -0,0 +1,21 @@
+<template>
+	<code>
+		<slot />
+	</code>
+</template>
+
+<style scoped>
+code {
+	border-radius: 3px;
+	padding: 0.2em 0.4em;
+	border-radius: 3px;
+	font-size: 85%;
+	tab-size: 2;
+	color: #eb5757;
+	background-color: rgba(135, 131, 120, 0.15);
+	caret-color: rgb(55, 53, 47);
+}
+.dark code {
+	caret-color: rgba(255, 255, 255, 0.81);
+}
+</style>
